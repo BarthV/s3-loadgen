544699d3e27201cff0140f3ad7015122be084f5d
Barthelemy Vessemont <b.vessemont@criteo.com>
2019-05-07 11:23:33 +0200

    measure write ops/err/latency

--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
 	"os"
 	"path"
 	"time"
 
-	"github.com/Pallinder/go-randomdata"
 	"github.com/minio/minio-go"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	log "github.com/sirupsen/logrus"
 )
@@ -25,12 +26,50 @@ var (
 	writeBucketName = "s3-loadgen-writes"
 	readBucketName  = "s3-loadgen-reads"
 	location        = "par"
+
+	writeOpCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "s3_write_counter",
+		Help: "s3 writes operations counter.",
+	})
+	writeErrCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "s3_write_err_counter",
+		Help: "s3 writes errors counter.",
+	})
+	readOpCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "s3_read_counter",
+		Help: "s3 reads operations counter.",
+	})
+	readErrCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "s3_read_err_counter",
+		Help: "s3 read errors counter.",
+	})
+	writeDurationsHistogram = prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name:    "s3_write_durations_histogram_seconds",
+		Help:    "S3 write operations latency distributions.",
+		Buckets: []float64{0.01, 0.0125, 0.015, 0.0175, 0.02, 0.025, 0.03, 0.04, 0.05, 0.075, 0.1, 0.125, 0.15, 0.175, 0.2, 0.25, 0.3, 0.4, 0.5},
+	})
+	readDurationsHistogram = prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name:    "s3_read_durations_histogram_seconds",
+		Help:    "S3 read operations latency distributions.",
+		Buckets: []float64{0.01, 0.0125, 0.015, 0.0175, 0.02, 0.025, 0.03, 0.04, 0.05, 0.075, 0.1, 0.125, 0.15, 0.175, 0.2, 0.25, 0.3, 0.4, 0.5},
+	})
 )
+
+const letterBytes = "\nabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\n"
 
 func usage() {
 	fmt.Fprintf(os.Stderr, "Usage:\n")
 	fmt.Fprintf(os.Stderr, "  ./s3-loadgen -e <HOST:PORT> -a <S3_ID> -k <S3_secret> [-s] [-l <loglevel>]\n")
 	os.Exit(2)
+}
+
+func initPrometheus() {
+	prometheus.MustRegister(writeOpCounter)
+	prometheus.MustRegister(writeErrCounter)
+	prometheus.MustRegister(writeDurationsHistogram)
+	prometheus.MustRegister(readOpCounter)
+	prometheus.MustRegister(readErrCounter)
+	prometheus.MustRegister(readDurationsHistogram)
 }
 
 func initLogger() {
@@ -49,40 +88,8 @@ func initLogger() {
 	}
 }
 
-func writeRndObject(minioClient *minio.Client) {
-	tmpfile, err := ioutil.TempFile("/tmp/", "s3-loadgen-")
-	defer tmpfile.Close()
-	defer os.Remove(tmpfile.Name())
-
-	if err != nil {
-		log.Error("Cannot create temporary file: ", err)
-		return
-	}
-
-	if _, err = tmpfile.Write([]byte(randomdata.Paragraph())); err != nil {
-		log.Fatal("Failed to write to temporary file", err)
-	}
-
-	n, err := minioClient.FPutObject(writeBucketName, path.Base(tmpfile.Name()), tmpfile.Name(), minio.PutObjectOptions{ContentType: "text/plain"})
-	if err != nil {
-		log.Error("Cannot create temporary file: ", err)
-		return
-	}
-	log.Debugf("Object stored: %s - %d", path.Base(tmpfile.Name()), n)
-}
-
-func writeObjectLoop(minioClient *minio.Client) {
-	tickerWrites := time.NewTicker(250 * time.Millisecond)
-	for {
-		select {
-		case <-tickerWrites.C:
-			go writeRndObject(minioClient)
-		}
-	}
-}
-
 func prepareBuckets(minioClient *minio.Client) {
-	// Make a new bucket for reads and writes operations.
+	// Verify & creates new buckets for reads and writes operations.
 	// Create write bucket
 	err := minioClient.MakeBucket(writeBucketName, location)
 	if err != nil {
@@ -112,11 +119,81 @@ func prepareBuckets(minioClient *minio.Client) {
 	}
 }
 
+func fillReadBucket(minioClient *minio.Client) {
+	for i := 1; i <= 200; i++ {
+		writeObjectWithID(i, minioClient)
+	}
+}
+
+func randomAlphaString(len int) string {
+	b := make([]byte, len)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(54)]
+	}
+	return string(b)
+}
+
+func writeObjectWithID(id int, minioClient *minio.Client) {
+	tmpfile, err := ioutil.TempFile("/tmp/", "s3-loadgen-")
+	defer tmpfile.Close()
+	defer os.Remove(tmpfile.Name())
+
+	objectNameWithID := fmt.Sprintf("s3-loadgen-%d", id)
+
+	if err != nil {
+		log.Error("Cannot create temporary file: ", err)
+		return
+	}
+
+	if _, err = tmpfile.Write([]byte(randomAlphaString(250000))); err != nil {
+		log.Fatal("Failed to write to temporary file", err)
+	}
+
+	n, err := minioClient.FPutObject(readBucketName, objectNameWithID, tmpfile.Name(), minio.PutObjectOptions{ContentType: "text/plain"})
+	if err != nil {
+		log.Error("Cannot create temporary file: ", err)
+		return
+	}
+	log.Debugf("Object stored in read bucket: %s (%dB) ", objectNameWithID, n)
+}
+
+func writeRndObject(minioClient *minio.Client) {
+	writeOpCounter.Inc()
+	tmpfile, err := ioutil.TempFile("/tmp/", "s3-loadgen-")
+	defer tmpfile.Close()
+	defer os.Remove(tmpfile.Name())
+
+	if err != nil {
+		log.Error("Cannot create temporary file: ", err)
+		writeErrCounter.Inc()
+		return
+	}
+
+	if _, err = tmpfile.Write([]byte(randomAlphaString(250000))); err != nil {
+		log.Fatal("Failed to write to temporary file", err)
+		writeErrCounter.Inc()
+		return
+	}
+
+	start := time.Now()
+	n, err := minioClient.FPutObject(writeBucketName, path.Base(tmpfile.Name()), tmpfile.Name(), minio.PutObjectOptions{ContentType: "text/plain"})
+	if err != nil {
+		log.Error("Cannot create temporary file: ", err)
+		writeErrCounter.Inc()
+		return
+	}
+	writeDuration := time.Since(start)
+	writeDurationsHistogram.Observe(writeDuration.Seconds())
+
+	log.Debugf("Object stored: %s (%dB) in %f seconds", path.Base(tmpfile.Name()), n, writeDuration.Seconds())
+}
+
 func main() {
 	flag.Usage = usage
 	flag.Parse()
 
 	initLogger()
+	initPrometheus()
 
 	// Initialize Prometheus metrics endpoint
 	log.Info("Serving Prometheus metrics endpoint at :9090")
@@ -134,11 +211,19 @@ func main() {
 	log.Debugf("%#v\n", minioClient) // Debug minioClient instance
 
 	prepareBuckets(minioClient)
+	fillReadBucket(minioClient)
 
-	go writeObjectLoop(minioClient)
-	// writeRndObject(minioClient)
+	// go writeObjectLoop(minioClient)
+	writeRndObject(minioClient)
 
-	// lol :troll:
-	endlessWait := make(chan struct{})
-	<-endlessWait
+	tickerWrites := time.NewTicker(250 * time.Millisecond)
+	tickerReads := time.NewTicker(100 * time.Millisecond)
+	for {
+		select {
+		case <-tickerWrites.C:
+			go writeRndObject(minioClient)
+		case <-tickerReads.C:
+			// go readRndObject(minioClient)
+		}
+	}
 }
